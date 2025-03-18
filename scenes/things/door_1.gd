38e6d1122a686dd2addf623176767ffdfda33c3e
grutegz <123324300+grutegz@users.noreply.github.com>
2025-03-18 14:53:00 +0500

    base door logic

---
+++ scenes/things/door_1.gd
@@ -0,0 +1,23 @@
+extends StaticBody3D
+
+var open = true
+
+const OPEN_R = Vector3(-1, 2, 0)
+const OPEN_L = Vector3(5, 2, 0)
+const SPEED = 5.0
+
+@onready var door_r: Node3D = $colr
+@onready var door_l: Node3D = $coll
+
+var closed_r: Vector3
+var closed_l: Vector3
+func _ready() -> void:
+	closed_r = door_r.position
+	closed_l = door_l.position
+
+func _process(delta: float) -> void:
+	var target_r = OPEN_R if open else closed_r
+	var target_l = OPEN_L if open else closed_l
+
+	door_r.position = door_r.position.lerp(target_r, 1 - exp(-SPEED * delta))
+	door_l.position = door_l.position.lerp(target_l, 1 - exp(-SPEED * delta))
